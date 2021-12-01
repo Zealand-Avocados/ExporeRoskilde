f1d499f9437495ee1b1ac6bcc548466a601ff106
Mohammed Bilal Al-Shalan <moha151h@edu.easj.dk>
2021-12-01 13:16:08 +0100

    hi

--- ExploreRoskilde/Pages/Places/AddPlace.cshtml.cs
+++ ExploreRoskilde/Pages/Places/AddPlace.cshtml.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ExploreRoskilde.Models;
 
 namespace ExploreRoskilde.Pages.Places
 {
     public class AddPlace : PageModel
     {
+        public Place Place { get; set; } 
+
         public void OnGet()
         {
             
