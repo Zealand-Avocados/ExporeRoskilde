87a5c499c477bf5acf59e2265d4f1dfe044ac88c
Martin Holoubek <marholoubek@gmail.com>
2021-12-01 12:46:12 +0100

    move places to index.html

--- ExploreRoskilde/Pages/Index.cshtml.cs
+++ ExploreRoskilde/Pages/Index.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ExploreRoskilde.Interfaces;
+using ExploreRoskilde.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -11,10 +13,17 @@ namespace ExploreRoskilde.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        
+        public Dictionary<int, Place> AllPlaces {get; set; }
+        
+        IPlacesCatalog _placesCatalog;
+        
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IPlacesCatalog catalog)
         {
             _logger = logger;
+            _placesCatalog = catalog;
+            AllPlaces = _placesCatalog.AllPlaces();
         }
 
         public void OnGet()
