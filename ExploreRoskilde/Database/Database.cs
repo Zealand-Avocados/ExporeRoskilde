1a295f207d553ac33a9bd87e9c07198c2ccbdbfe
AdamCipkala <acipkala@gmail.com>
2021-12-01 10:13:57 +0100

    Database

---
+++ ExploreRoskilde/Database/Database.cs
@@ -0,0 +1,26 @@
+﻿using ExploreRoskilde.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExploreRoskilde.Database
+{
+    public static class Database
+    {
+        private static string _filePath = "Places.json";
+
+        public static void WriteToJson(string filePath, Dictionary<int, Place> places)
+        {
+            string output = System.Text.Json.JsonSerializer.Serialize(places);
+            File.WriteAllText(filePath, output);
+        }
+
+        public static Dictionary<int, Place> ReadJson(string filePath)
+        {
+            string jsonString = File.ReadAllText(filePath);
+            return System.Text.Json.JsonSerializer.Deserialize<Dictionary<int, Place>>(jsonString);
+        }
+    }
+}
--- ExploreRoskilde/Repositories/PlacesCatalog.cs
+++ ExploreRoskilde/Repositories/PlacesCatalog.cs
@@ -1,13 +1,15 @@
 using System.Collections.Generic;
 using ExploreRoskilde.Interfaces;
 using ExploreRoskilde.Models;
+using ExploreRoskilde.Database;
+
 
 namespace ExploreRoskilde.Repositories
 {
     public class PlacesCatalog : IPlacesCatalog
     {
-        
-        
+        //public Dictionary<int, Place> allPLaces => 
+
         
     }
 }
