95ddc604e8ef0709412f8e43dd0d9c5aded8e887
Martin Holoubek <marholoubek@gmail.com>
2021-12-03 10:19:26 +0100

    fake database

--- ExploreRoskilde/Database/Database.cs
+++ ExploreRoskilde/Database/Database.cs
@@ -1,4 +1,5 @@
-﻿using ExploreRoskilde.Models;
+﻿using System;
+using ExploreRoskilde.Models;
 using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
@@ -10,18 +11,24 @@ namespace ExploreRoskilde.Database
     public static class Database
     {
         public static string PlacesFilePath = "Database/Places.json";
+        
+        private static Dictionary<int, Place> _places = new Dictionary<int, Place>();
 
         public static void WriteToJson(string filePath, Dictionary<int, Place> places)
         {
             //  string output = System.Text.Json.JsonSerializer.Serialize(places);
-            string output = JsonConvert.SerializeObject(places);
-            File.WriteAllText(filePath, output);
+            // var output = JsonConvert.SerializeObject(places);
+            // string output = "{:::::: }";
+            // File.WriteAllText(filePath, output);
+            _places = places;
         }
 
         public static Dictionary<int, Place> ReadJson(string filePath)
         {
-            var jsonString = File.ReadAllText(filePath);
-            return JsonConvert.DeserializeObject<Dictionary<int, Place>>(jsonString);
+            // var jsonString = File.ReadAllText(filePath);
+            // return JsonConvert.DeserializeObject<Dictionary<int, Place>>(jsonString);
+            
+            return _places;
         }
     }
 }
