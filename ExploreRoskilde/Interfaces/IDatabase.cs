82e8beffa4b49e1b0a46fe7cb494adb9e23338bd
Mohammed Bilal Al-Shalan <moha151h@edu.easj.dk>
2021-12-07 10:39:33 +0100

    validation 0.7

--- ExploreRoskilde/Interfaces/IDatabase.cs
+++ ExploreRoskilde/Interfaces/IDatabase.cs
@@ -1,6 +1,15 @@
-﻿namespace ExploreRoskilde.Interfaces
+﻿using ExploreRoskilde.Models;
+using System.Collections.Generic;
+
+namespace ExploreRoskilde.Interfaces
 {
     public interface IDatabase
     {
+        public interface IDatabase
+        {
+            public void WriteToJson(string filePath, Dictionary<int, Place> places);
+            public Dictionary<int, Place> ReadJson(string filePath);
+
+        }
     }
 }
