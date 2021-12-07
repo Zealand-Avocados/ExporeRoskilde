d382f999fb016d734d44aa723d6083cbeed5be8a
Mohammed Bilal Al-Shalan <moha151h@edu.easj.dk>
2021-12-07 10:25:52 +0100

    validation 0.1

--- ExploreRoskilde/Models/User.cs
+++ ExploreRoskilde/Models/User.cs
@@ -1,6 +1,22 @@
-﻿namespace ExploreRoskilde.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExploreRoskilde.Models
 {
-    public class User
-    {
+            public class User
+        {
+            [Required] [MinLength(1)] 
+            public int UserId { get; set; }
+
+            [Required] [MinLength(4)]
+            public string Name { get; set; }
+
+            [Required]
+            public string Email { get; set; }
+
+            [Required] [MinLength(8)] 
+            public string Password { get; set; }
+            public bool IsAdmin { get; set; }
+        }
+
     }
-}
+
