1e57f642bcbff70d61f5203d3895aa67fe9ac5da
Patrik Hoferica <patohoferica1@gmail.com>
2021-12-07 11:26:00 +0100

    redirecting between pages

--- ExploreRoskilde/Pages/Places/RemovePlace.cshtml.cs
+++ ExploreRoskilde/Pages/Places/RemovePlace.cshtml.cs
@@ -1,12 +1,30 @@
+using System;
+using ExploreRoskilde.Interfaces;
+using ExploreRoskilde.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ExploreRoskilde.Pages.Places
 {
     public class RemovePlace : PageModel
     {
-        public void OnGet()
+        
+        
+        public Place Place { get; set; }
+        
+        private IPlacesCatalog placesCatalog;
+
+        public RemovePlace(IPlacesCatalog catalog)
         {
-            
+            placesCatalog = catalog;
+        }
+        
+        
+        public IActionResult OnGet(String id)
+        {
+            Place = placesCatalog.GetPlaceById(id);
+            if (Place == null) return RedirectToPage("/NotFound");
+            return Page();
         }
     }
 }
--- ExploreRoskilde/Pages/Places/UpdatePlace.cshtml.cs
+++ ExploreRoskilde/Pages/Places/UpdatePlace.cshtml.cs
@@ -1,12 +1,28 @@
+using System;
+using ExploreRoskilde.Interfaces;
+using ExploreRoskilde.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ExploreRoskilde.Pages.Places
 {
     public class UpdatePlace : PageModel
     {
-        public void OnGet()
+        public Place Place { get; set; }
+        
+        private IPlacesCatalog catalog;
+
+        public UpdatePlace(IPlacesCatalog catalogPlaces)
         {
-            
+            catalog = catalogPlaces;
+        }
+        
+        public IActionResult OnGet(String id)
+        {
+            Place = catalog.GetPlaceById(id);
+            if (Place == null) return RedirectToPage("/NotFound");
+            return Page();
+
         }
     }
 }
