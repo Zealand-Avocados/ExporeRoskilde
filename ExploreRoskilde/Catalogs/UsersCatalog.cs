d2a984ee0c55b4a517606aae2c1f310a8c29d04d
Mohammed Bilal Al-Shalan <moha151h@edu.easj.dk>
2021-12-07 10:52:41 +0100

    validation 1.0

--- ExploreRoskilde/Catalogs/UsersCatalog.cs
+++ ExploreRoskilde/Catalogs/UsersCatalog.cs
@@ -1,6 +1,25 @@
-﻿namespace ExploreRoskilde.Catalogs
+﻿using ExploreRoskilde.Interfaces;
+using ExploreRoskilde.Models;
+using System.Collections.Generic;
+
+namespace ExploreRoskilde.Catalogs
 {
-    public class Class
-    {
+   
+        public class UsersCatalog : IUsersCatalog
+        {
+            public Dictionary<int, User> AllUsers() => Database.Database.ReadJsonUsers(Database.Database.PlacesFilePath);
+
+            public User GetUserById(int id)
+            {
+                return AllUsers()[id];
+            }
+
+
+            public void AddUser(User user)
+            {
+                Dictionary<int, User> users = AllUsers();
+                users.Add(user.UserId, user);
+                Database.Database.WriteToJsonUsers(Database.Database.PlacesFilePath, users);
+            }
+        }
     }
-}
