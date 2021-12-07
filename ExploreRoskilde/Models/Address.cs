7846c5a24daf80a9020d1e2b0f684dd62a9521ba
Mohammed Bilal Al-Shalan <moha151h@edu.easj.dk>
2021-12-07 11:21:48 +0100

    validation 1.4

--- ExploreRoskilde/Models/Address.cs
+++ ExploreRoskilde/Models/Address.cs
@@ -4,25 +4,29 @@ namespace ExploreRoskilde.Models
 {
     public class Address
     {
-        [Required]
-        private string _street { get; }
 
-        [Required]
-        private string _city { get; }
-
-        [Required]
-        private string _zipcode { get; }
-
-        [Required]
-        private string _country { get; }
-
-        public Address (string street, string city, string zipcode, string county)
+        public Address(string street, string city, string zipcode, string county)
         {
-            _street = street;
-            _city = city;
-            _zipcode = zipcode;
-            _country = county;
+            Street = street;
+            City = city;
+            Zipcode = zipcode;
+            Country = county;
         }
 
+        public Address()
+        {
+        }
+
+        [Required]
+        public string Street { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Zipcode { get; set; }
+        [Required]
+        public string Country { get; set; }
+
+
+        public override string ToString() => $"{Street}, {City}, {Zipcode}, {Country}";
     }
 }
