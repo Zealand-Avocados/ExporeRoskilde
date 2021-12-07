b8bda660c8e568577b88314628dfbfd2a6406814
Patrik Hoferica <patohoferica1@gmail.com>
2021-12-07 10:10:38 +0100

    add UpdatePage

---
+++ ExploreRoskilde/Pages/Places/UpdatePlace.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ExploreRoskilde.Pages.Places
+{
+    public class UpdatePlace : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
