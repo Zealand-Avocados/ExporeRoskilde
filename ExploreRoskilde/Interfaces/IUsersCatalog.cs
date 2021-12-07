c87479b8c33b3e0b2571cb0aa5d5b3acf1351e4f
Mohammed Bilal Al-Shalan <moha151h@edu.easj.dk>
2021-12-07 10:59:21 +0100

    validation 1.1

--- ExploreRoskilde/Interfaces/IUsersCatalog.cs
+++ ExploreRoskilde/Interfaces/IUsersCatalog.cs
@@ -3,13 +3,10 @@ using System.Collections.Generic;
 
 namespace ExploreRoskilde.Interfaces
 {
-    public interface IUsersCatalog
-    {
         public interface IUsersCatalog
         {
             public Dictionary<int, User> AllUsers();
             public User GetUserById(int id);
             public void AddUser(User place);
         }
-    }
 }
