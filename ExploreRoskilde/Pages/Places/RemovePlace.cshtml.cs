c7c4cbc4ad6fa173155e8d6d45fbaf17ac6df522
Patrik Hoferica <patohoferica1@gmail.com>
2021-12-07 10:02:29 +0100

    add remove page

---
+++ ExploreRoskilde/Pages/Places/RemovePlace.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ExploreRoskilde.Pages.Places
+{
+    public class RemovePlace : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
