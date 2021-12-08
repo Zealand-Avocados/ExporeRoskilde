68720b52c702094ca5440e5334147eba3325d678
Mohammed Bilal Al-Shalan <moha151h@edu.easj.dk>
2021-12-08 10:49:14 +0100

    Reuglar Expressions

--- ExploreRoskilde/Models/User.cs
+++ ExploreRoskilde/Models/User.cs
@@ -5,20 +5,25 @@ namespace ExploreRoskilde.Models
     public class User
     {
         [Required(ErrorMessage = "Username is required")]
-        [MinLength(1)] 
+        [StringLength(16, ErrorMessage = "Must be between 5 and 16 characters", MinimumLength = 5)]
+        [RegularExpression("^ [a-zA-Z0-9] *$", ErrorMessage = "")]
         public int UserId { get; set; }
 
-            [Required] [MinLength(4)]
+       [Required] [MinLength(4)]
+       [DataType(DataType.Text)]
+       
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Email is required")]
         [DataType(DataType.EmailAddress)]
         [EmailAddress(ErrorMessage = "Email is incorrect, please try again")]
+        
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
         [MinLength(8)]
         [DataType(DataType.Password)]
+        
         
         public string Password { get; set; }
         public bool IsAdmin { get; set; }
