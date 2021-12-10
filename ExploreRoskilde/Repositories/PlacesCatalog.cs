0997422ceec51c12d652078a21eb7419b6d7055a
Patrik Hoferica <patohoferica1@gmail.com>
2021-12-10 11:04:09 +0100

    merge main, making changes

--- ExploreRoskilde/Repositories/PlacesCatalog.cs
+++ ExploreRoskilde/Repositories/PlacesCatalog.cs
@@ -24,15 +24,13 @@ namespace ExploreRoskilde.Repositories
             places.Add(place.Id, place);
             Database.Database.WriteToJsonPlaces(places);
         }
-        
+
         public void DeletePlace(Place place)
         {
             Dictionary<string, Place> places = AllPlaces();
             places.Remove(place.Id);
             Database.Database.WriteToJsonPlaces(places);
         }
-        
-        
 
 
         public void UpdatePlace(Place place)
@@ -49,20 +47,17 @@ namespace ExploreRoskilde.Repositories
                 i.ImageUrl = place.ImageUrl;
                 break;
             }
+
             Database.Database.WriteToJsonPlaces(places);
         }
-        
-        
-        
-        
-        
-       
+
+
         public Dictionary<string, Place> Searching(string searching, int category)
         {
             Dictionary<string, Place> places = AllPlaces();
             Dictionary<string, Place> filteredPlaces = new Dictionary<string, Place>();
 
-            if ((string.IsNullOrEmpty(searching) == false) && (category < 1)) 
+            if ((string.IsNullOrEmpty(searching) == false) && (category < 1))
             {
                 foreach (var place in places)
                 {
@@ -74,26 +69,23 @@ namespace ExploreRoskilde.Repositories
 
                 return filteredPlaces;
             }
-            
-            else if(string.IsNullOrEmpty(searching) && (category > 0)) 
-            {
 
+            else if (string.IsNullOrEmpty(searching) && (category > 0))
+            {
                 foreach (var place in places)
                 {
-                    if ((int)place.Value.Category == category)
+                    if ((int) place.Value.Category == category)
                         filteredPlaces.Add(place.Key, place.Value);
                 }
-                    
+
                 return filteredPlaces;
             }
 
-            else if((string.IsNullOrEmpty(searching) == false) && (category > 0))
-            {   
-                
+            else if ((string.IsNullOrEmpty(searching) == false) && (category > 0))
+            {
                 foreach (var place in places)
                 {
-
-                    if(place.Value.Title.StartsWith(searching) && (int)place.Value.Category == category)
+                    if (place.Value.Title.StartsWith(searching) && (int) place.Value.Category == category)
                         filteredPlaces.Add(place.Key, place.Value);
                 }
 
@@ -105,6 +97,5 @@ namespace ExploreRoskilde.Repositories
                 return places;
             }
         }
-        
     }
 }
